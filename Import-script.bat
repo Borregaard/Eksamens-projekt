8a29bf9221f7eda673f5c221420316c0a32e59f2
borregaard <frederik.borregaard@gmail.com>
2022-02-24 11:26:21 +0100

    Import batch file as been added

---
+++ Import-script.bat
@@ -0,0 +1,8 @@
+start pip install plotly
+start pip install dash
+start pip install yfinance
+start pip install tabulate
+start pip install tqdm
+start pip install matplotlib
+echo "All libaries are installed"
+pause
